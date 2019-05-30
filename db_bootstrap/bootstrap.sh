836e7e6b8f05dde78cc94a23ee23cc655c7f6935
Thomas Austin <thomasaustin@gmail.com>
2019-05-30 23:11:38 +0100

    bootstrap db

---
+++ db_bootstrap/bootstrap.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sleep 60
+
+sqlplus -s /nolog <<EOF
+  connect system/Oradoc_db1@db:1521/ORCLCDB.localdomain
+  alter system set archive_lag_target=0 scope=both;
+  drop table things_staging PURGE;
+  create table things_staging ( id integer, thing varchar2(50) );
+  quit
+EOF
--- scheduled_job/load.sh
+++ scheduled_job/load.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-sqlplus -s /nolog <<EOF
-  connect system/Oradoc_db1@db:1521/ORCLCDB.localdomain
-  alter system set archive_lag_target=0 scope=both;
-  drop table things_staging PURGE;
-  create table things_staging ( id integer, thing varchar2(50) );
-  quit
-EOF
-
 /usr/lib/oracle/19.3/client64/bin/sqlldr system/Oradoc_db1@db:1521/ORCLCDB.localdomain control=/app/things.ctl log=/app/sqlldr.log bad=/app/sqlldr.bad
 
 cat /app/sqlldr.log
