4d2c9d43e399a65f399b92a9ddd5c2c625f1aa16
Thomas Austin <thomasaustin@gmail.com>
2019-05-30 15:12:04 +0100

    report the import

--- scheduled_job/load.sh
+++ scheduled_job/load.sh
@@ -7,4 +7,6 @@ sqlplus -s /nolog <<EOF
   quit
 EOF
 
-/usr/lib/oracle/19.3/client64/bin/sqlldr system/Oradoc_db1@db:1521/ORCLCDB.localdomain control=/app/things.ctl
+/usr/lib/oracle/19.3/client64/bin/sqlldr system/Oradoc_db1@db:1521/ORCLCDB.localdomain control=/app/things.ctl log=/app/sqlldr.log bad=/app/sqlldr.bad
+
+cat /app/sqlldr.log
