48692cf0403896f9d0804faabf9af07e3fb56cc7
Thomas Austin <thomasaustin@gmail.com>
2019-05-30 19:36:53 +0100

    depend on health of db

--- scheduled_job/load.sh
+++ scheduled_job/load.sh
@@ -2,8 +2,9 @@
 
 sqlplus -s /nolog <<EOF
   connect system/Oradoc_db1@db:1521/ORCLCDB.localdomain
-  create table things_staging ( id integer, thing varchar2(20) );
-  delete from things_staging;
+  alter system set archive_lag_target=0 scope=both;
+  drop table things_staging PURGE;
+  create table things_staging ( id integer, thing varchar2(50) );
   quit
 EOF
 
